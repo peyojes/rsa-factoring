ecbd396b10f8ccada1ee38af7a5f7445b630df7f
pawel <pawelcio1996@gmail.com>
2018-07-28 19:41:40 +0200

    Added NextPrime and IsReletivelyPrime methods.

--- include/number.h
+++ include/number.h
@@ -5,6 +5,9 @@
 
 #include "InfInt.h"
 
+const int kTwo = 2;
+const int kZero = 0;
+
 class Number : public InfInt {
  public:
   Number(): InfInt() {}
@@ -14,14 +17,40 @@ class Number : public InfInt {
   Number(const InfInt& number): InfInt(number) {}
 
   bool IsPrime();
+  void NextPrime();
+  bool IsRelativelyPrime(const Number &num);
 };
 
 bool Number::IsPrime() {
-  Number it(2), tmp;
+  Number it(kTwo), tmp, sqrt = intSqrt();
   bool result = true;
-  for (; it < this->operator /( it); it++ ) {
-    tmp = this->operator%( it);
-    if (tmp == 0) {
+  sqrt++;
+  for (; it <= sqrt; it++) {
+    tmp = *this % it;
+    if (tmp == kZero) {
+      result = false;
+      break;
+    }
+  }
+
+  return result;
+}
+
+void Number::NextPrime() {
+  for (; !this->IsPrime();this->operator++());
+}
+
+bool Number::IsRelativelyPrime(const Number &num) {
+  Number greater_num = *this < num ? *this: num;
+  Number limit_num = greater_num / kTwo;
+  Number it(kTwo), modulo_result_this, modulo_result_num;
+  bool result = true;
+
+  for (;it <= limit_num; it++) {
+    modulo_result_this = *this % it;
+    modulo_result_num = num % it;
+
+    if (modulo_result_num == 0 && modulo_result_this == 0) {
       result = false;
       break;
     }
