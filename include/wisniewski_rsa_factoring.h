486269be5a1ebbbbee6dc9f1e309160eeb45838c
pawel <pawelcio1996@gmail.com>
2018-11-06 20:47:04 +0100

    Fixed problem witj WisniewskiRsaFactoring class.

--- include/wisniewski_rsa_factoring.h
+++ include/wisniewski_rsa_factoring.h
@@ -4,18 +4,20 @@
 #include "rsa_data.h"
 
 #include <vector>
+#include <iostream>
 
 class WisniewskiRsaFactoring : public FactoringMethod {
  public:
   void Factoring();
  private:
-  void CalculatePQAndPrivateKey(const Number& x);
-  bool IsFound(const Number* mod_results);
+  void CalculatePQAndPrivateKey(const Number& x, const int number_of_equation);
+  bool IsFound(const Number* mod_results, int *number_of_equation);
 };
 
 inline void WisniewskiRsaFactoring::Factoring() {
   Number  first_member[4], second_member[4], modulo_result[4], x(1), six(6);
   bool is_find = false;
+  int number_of_equation = -1;
   while (!is_find) {
     x++;
 
@@ -27,29 +29,37 @@ inline void WisniewskiRsaFactoring::Factoring() {
     second_member[2] = GetModulus() - (six * x) + 1;
     second_member[3] = GetModulus() + (six * x) - 1;
 
-    modulo_result[0] = first_member[0] % second_member[0];
-    modulo_result[1] = first_member[0] % second_member[1];
-    modulo_result[2] = first_member[1] % second_member[2];
-    modulo_result[3] = first_member[1] % second_member[3];
+    modulo_result[0] = second_member[0] % first_member[0];
+    modulo_result[1] = second_member[1] % first_member[0];
+    modulo_result[2] = second_member[2] % first_member[1];
+    modulo_result[3] = second_member[3] % first_member[1];
 
-    is_find = IsFound(modulo_result);
+    is_find = IsFound(modulo_result, &number_of_equation);
   }
-  CalculatePQAndPrivateKey(x);
+  CalculatePQAndPrivateKey(x, number_of_equation);
 }
 
-inline void WisniewskiRsaFactoring::CalculatePQAndPrivateKey(const Number &x) {
-  p_ = x;
+inline void WisniewskiRsaFactoring::CalculatePQAndPrivateKey(
+    const Number &x, const int number_of_equation) {
+
+  if (number_of_equation <= 1 && number_of_equation >= 0) {
+    p_ = x * 6 + 1;
+  } else if (number_of_equation >= 2 && number_of_equation <=3) {
+    p_ = x * 6 - 1;
+  }
+
   q_ = GetModulus() / p_;
   CalculateEulerFunction();
   FindPrivateKey();
 }
 
-inline bool WisniewskiRsaFactoring::IsFound(const Number *mod_results) {
+inline bool WisniewskiRsaFactoring::IsFound(const Number *mod_results,
+                                            int *number_of_equation) {
   bool result = false;
-
   for (int it = 0; it < 4; it++) {
     if (mod_results[it] == Number(0)) {
       result = true;
+      *number_of_equation = it;
     }
   }
   return result;
--- test/wisniewski_rsa_factoring_test.cc
+++ test/wisniewski_rsa_factoring_test.cc
@@ -9,18 +9,19 @@ TEST(WisniewskiRsaFactoring, emptyInitialization) {
 
 TEST(WisniewskiRsaFactoring, factoring) {
   WisniewskiRsaFactoring factoring;
-  factoring.SetModulus(143);
-  factoring.SetPublicKey(17);
+  factoring.SetModulus(5959);
+  factoring.SetPublicKey(777);
   factoring.Factoring();
   int  p(factoring.GetP().toInt());
   int  q(factoring.GetQ().toInt());
   bool result = false;
-  if ((p == 13 && q == 11) ||(p == 11 && q == 13)) {
+
+  if ((p == 59 && q == 101) ||(p == 101 && q == 59)) {
     result = true;
   }
 
   EXPECT_TRUE(result);
-  EXPECT_EQ(120, factoring.GetValueOfEulerFunction().toInt());
-  EXPECT_EQ(113, factoring.GetPrivateKey().toInt());
-  EXPECT_EQ(17, factoring.GetPublicKey().toInt());
+  EXPECT_EQ(5800, factoring.GetValueOfEulerFunction().toInt());
+  EXPECT_EQ(4113, factoring.GetPrivateKey().toInt());
+  EXPECT_EQ(777, factoring.GetPublicKey().toInt());
 }
