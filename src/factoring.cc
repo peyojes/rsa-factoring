9e69d0c0268e8ea1b5d6345a8223c7e27af1c06d
pawel <pawelcio1996@gmail.com>
2018-11-08 20:21:26 +0100

    Added returning result to file.

--- src/factoring.cc
+++ src/factoring.cc
@@ -1,5 +1,8 @@
 #include <iostream>
 #include <string>
+#include <stdio.h>
+#include <time.h>
+#include <fstream>
 
 #include "../include/csv.h"
 #include "../include/factoring_method.h"
@@ -13,12 +16,37 @@ using std::cout;
 using std::string;
 using io::CSVReader;
 using cxxtimer::Timer;
+using std::chrono::seconds;
+
+const string CurrentDateTime();
+
+class FactoringResult {
+ public:
+  FactoringResult(const Number& modulus, const Number& p, const Number& q,
+                  const int64_t time);
+  string GetLineToCsv();
+ private:
+  Number modulus_;
+  Number p_;
+  Number q_;
+  int64_t time_;
+};
+
+class FileSaver {
+ public:
+  void SetFileName(const std::string& file);
+  void SaveToFile(std::vector<FactoringResult>& results);
+ private:
+  string file_;
+};
+
 int main(int argc, char *argv[]) {
   FactoringMethod *factoring;
-  if (argc != 3) {
+  if (argc != 4) {
     cout << "Wrong input parameters!\n"
-              "\tusage: a.out method_factoring file.csv\n"
-            "method_factoring: fermat pollard simple_beg simple_mid wisniewski\n";
+              "\tusage: a.out method_factoring file.csv save_flag\n"
+            "method_factoring: fermat pollard simple_beg simple_mid wisniewski\n"
+            "save_flag: true or false\n";
     return 0;
   }
   string method_factoring(argv[1]);
@@ -43,21 +71,80 @@ int main(int argc, char *argv[]) {
     cout << "Wronge name of factoring method\n";
     return 0;
   }
+  bool save_to_file = false;
+  string save_flag(argv[3]);
+  if (save_flag == "true") {
+    save_to_file = true;
+  }
   Timer timer;
   CSVReader<3> in(argv[2]);
   in.read_header(
         io::ignore_extra_column, "rsa_number","p", "q");
   string rsa_number, p, q;
+  std::vector<FactoringResult> results;
+  FileSaver file_saver;
+
+  string file_name = method_factoring + "_" + CurrentDateTime() + ".csv";
+  file_saver.SetFileName(file_name);
   while (in.read_row(rsa_number, p, q)) {
     timer.reset();
     timer.start();
     factoring->SetModulus(rsa_number);
     factoring->Factoring();
     timer.stop();
+    FactoringResult result(rsa_number,factoring->GetP(), factoring->GetQ(),
+                           timer.count<seconds>());
+    results.push_back(result);
     cout << "It has taken " << timer.count<std::chrono::seconds>() << " seconds." << std::endl;
     cout << "P: " << p << "\t" << factoring->GetP() << "\n";
     cout << "Q: " << q << "\t" << factoring->GetQ() << "\n";
-//    cout << rsa_number << "\t" << p << "\t" << q << "\n";
+    cout << "current data time " << CurrentDateTime() << "\n";
+    if (save_to_file) {
+      file_saver.SaveToFile(results);
+      file_saver.SaveToFile(results);
+    }
   }
+
+
   return 0;
+}
+
+const string CurrentDateTime() {
+    time_t     now = time(0);
+    struct tm  tstruct;
+    char       buf[80];
+    tstruct = *localtime(&now);
+    strftime(buf, sizeof(buf), "%d-%m-%Y_%X", &tstruct);
+
+    return buf;
+}
+
+FactoringResult::FactoringResult(const Number &modulus,
+    const Number &p, const Number &q, const int64_t time) {
+  modulus_ = modulus;
+  p_ = p;
+  q_ = q;
+  time_ = time;
+}
+
+std::string FactoringResult::GetLineToCsv() {
+  string result = modulus_.toString() + "," + p_.toString() + "," +
+      q_.toString() + "," + std::to_string(time_);
+  return result;
+}
+
+void FileSaver::SetFileName(const std::string &file) {
+  file_ = file;
+}
+
+void FileSaver::SaveToFile(std::vector<FactoringResult> &results) {
+  std::ofstream output_file(file_);
+  if (output_file.is_open()) {
+    output_file << "rsa_number,p,q,time\n";
+    for(FactoringResult result: results) {
+      output_file << result.GetLineToCsv() + "\n";
+    }
+    output_file.close();
+  }
+
 }
